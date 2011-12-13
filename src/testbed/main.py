87be2d137aafe6ebd474a637f023b78d4c01f21e
danieleb <danieleb@d6711dc6-5534-4683-ab01-b7cf45dfea6f>
2011-12-13 16:49:05 +0000

    Factored out game state
    - Made a GameState class to contain the game state
    - Changed main_loop to accept state
    - Added a way to restart the game by reinitializing the game state
    
    git-svn-id: https://www.it-stud.hiof.no/svn/master_h11_danieleb/trunk@24 d6711dc6-5534-4683-ab01-b7cf45dfea6f

--- src/testbed/main.py
+++ src/testbed/main.py
@@ -5,12 +5,55 @@ import math
 
 from chars import Cat, Dog
 
+
 KEY_MAP = {
     pygame.locals.K_UP: 'up',
     pygame.locals.K_DOWN: 'down',
     pygame.locals.K_LEFT: 'left',
     pygame.locals.K_RIGHT: 'right',
 }
+
+
+class GameState(object):
+    """Contains the state of the game
+    """
+
+    def __init__(self, screen):
+        """
+
+        Arguments:
+        - `screen`:
+        """
+        self.screen = screen
+        self.reinitialize()
+
+    def reinitialize(self, ):
+        """Reinitialize the game
+        """
+        background = pygame.Surface(screen.get_size())
+        self.background = background.convert()
+        self.background.fill((200,200,200))
+
+        self.font = pygame.font.Font(None, 35)
+        self.subfont = pygame.font.Font(None, 25)
+
+        self.dogs = []
+        self.nums = 5.0
+
+        space = self.screen.get_size()[0]/float(self.nums)
+        for i in range(int(self.nums)):
+            y = 175 - 75*math.sin(i*math.pi/(self.nums-1))
+            x = (space/2)+i*space
+            self.dogs.append(Dog((x,y)))
+
+        self.kitty = Cat((320,400))
+
+        self.clock = pygame.time.Clock()
+        self.run = True
+        self.gameover = False
+        self.gamewin = False
+
+
 
 def handle_events():
     events = []
@@ -25,60 +68,57 @@ def handle_events():
             if event.key in KEY_MAP:
                 events.append({'type':'stop',
                                'dir':KEY_MAP[event.key]})
+        elif event.type == pygame.locals.MOUSEBUTTONDOWN:
+            events.append({'type':'click'})
     return events
 
-def main_loop(screen):
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    font = pygame.font.Font(None, 35)
 
-    dogs = []
-    nums = 5.0
+def main_loop(state):
 
-    for i in range(int(nums)):
-        space = screen.get_size()[0]/float(nums)
-        y = 175 - 75*math.sin(i*math.pi/(nums-1))
-        x = (space/2)+i*space
-        dogs.append(Dog((x,y)))
-
-    kitty = Cat((320,400))
-
-    clock = pygame.time.Clock()
-    run = True
-    gameover = False
-    gamewin = False
-
-    while run:
-        clock.tick(60)
+    while state.run:
+        state.clock.tick(60)
 
         events = handle_events()
         if 'quit' in [event['type'] for event in events]:
             return
 
-        background.fill((200,200,200))
-        screen.blit(background, (0,0))
+        state.screen.blit(state.background, (0,0))
 
-        if not gameover:
+        if not state.gameover:
             for event in events:
                 if event['type'] == 'move':
-                    kitty.move(event['dir'])
+                    state.kitty.move(event['dir'])
                 elif event['type'] == 'stop':
-                    kitty.stop(event['dir'])
+                    state.kitty.stop(event['dir'])
 
-            kitty.update()
+            state.kitty.update()
 
-            if kitty.get_rect().collidelist([dog.get_rect() for dog in dogs]) >= 0:
-                gameover = True
-                gamewin = False
-
-            kitty.draw(screen)
-            for dog in dogs:
+            state.kitty.draw(screen)
+            for dog in state.dogs:
                 dog.draw(screen)
+
+            kitty_rect = state.kitty.get_rect()
+            dog_rects = [dog.get_rect() for dog in state.dogs]
+            if kitty_rect.collidelist(dog_rects) >= 0:
+                state.gameover = True
+                state.gamewin = False
+                state.background.blit(state.screen, (0,0))
+                state.background.fill((64,64,64,64),special_flags=pygame.locals.BLEND_SUB)
+
         else:
-            message = "YOU WON!" if gamewin else "YOU LOST!"
-            text = font.render(message,True,[0,0,0])
+            for event in events:
+                if event['type'] == 'click':
+                    state.reinitialize()
+                    continue
+
+            message = "YOU WON!" if state.gamewin else "YOU LOST!"
+            text = state.font.render(message,True,[0,0,0])
             pos = text.get_rect(center = screen.get_rect().center)
-            screen.blit(text,pos)
+            state.screen.blit(text,pos)
+
+            subtext = state.subfont.render("Click to restart", True, [0,0,0])
+            pos = subtext.get_rect(midtop = pos.midbottom)
+            state.screen.blit(subtext,pos)
 
 
         pygame.display.flip()
@@ -88,7 +128,8 @@ pygame.init()
 
 screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption('Dead End')
+state = GameState(screen)
 
-main_loop(screen)
+main_loop(state)
 
 pygame.quit()
