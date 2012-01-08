f3e9cfc2f44269d09ddd8c9d9dcf0a394b33ee91
Daniel Bruce <ircubic@gmail.com>
2012-01-08 01:35:16 +0100

    Add control_ai to simulation.ai
    - This AI is intended to take external control through simulation.ai._cat_move,
      which should be set by the Simulation class.

--- src/testbed/simulation/ai.py
+++ src/testbed/simulation/ai.py
@@ -1,5 +1,15 @@
 import random
 
+_cat_move = ""
+
+def control_ai(cat, dogs, goal):
+    """Control AI.
+
+    This AI fetches the cat move set by the simulation's "setCatMove", allowing
+    for external control of the AI, such as through keyboard input.
+    """
+    return _cat_move
+
 
 def random_ai(cat, dogs, goal):
     """Randomly moving AI
