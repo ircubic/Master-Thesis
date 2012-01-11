9b0e5193a979caf19ce1b27aeb6f24b260e06025
Daniel Bruce <ircubic@gmail.com>
2012-01-11 16:58:03 +0100

    Implement follower AI for dogs

--- src/testbed/simulation/ai.py
+++ src/testbed/simulation/ai.py
@@ -16,3 +16,21 @@ def random_ai(current, cat, dogs, goal):
 
     """
     return random.choice(('left','right','up','down'))
+
+def follower_ai(current, cat, dogs, goal):
+    """Follower AI for Dog.
+
+    Tries to minimize the manhattan distance between self and cat.
+    """
+    diffx = cat[0] - current[0]
+    diffy = cat[1] - current[1]
+    if abs(diffx) > abs(diffy):
+        if diffx > 0:
+            return 'right'
+        else:
+            return 'left'
+    else:
+        if diffy > 0:
+            return 'down'
+        else:
+            return 'up'
