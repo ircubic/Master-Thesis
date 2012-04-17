886afa180e4e2972a689a26d7a744487b6ccf77c
Daniel Bruce <ircubic@gmail.com>
2012-04-17 10:44:05 -0500

    Added exit-achiever cat

--- src/testbed/simulation/ai.py
+++ src/testbed/simulation/ai.py
@@ -17,6 +17,24 @@ def random_ai(current, cat, dogs, goal):
     """
     return random.choice(('left','right','up','down'))
 
+def exit_achiever(current, cat, dogs, goal):
+    """Exit-achieving cat
+
+    """
+    x_diff = goal[0]-cat[0]
+    y_diff = goal[1]-cat[1]
+    if abs(x_diff) > abs(y_diff):
+        if x_diff > 0:
+            return 'right'
+        else:
+            return 'left'
+    else:
+        if y_diff > 0:
+            return 'down'
+        else:
+            return 'up'
+
+
 def follower_ai(current, cat, dogs, goal):
     """Follower AI for Dog.
 
